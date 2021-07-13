97c41e44a419b47aceb0918732738b56aa1253c9
Santhosh Kumar <RAVIKUMAR@RAVIKUMAR-PC>
2021-07-13 10:55:21 +0530

    viewBag,viewdata ,viewImport,viewstart

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Dynamic;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
@@ -10,7 +12,16 @@ namespace BookStore.Controllers
     {
         public ViewResult Index()
         {
-            
+            ViewBag.Title = "Site";
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.Name = "Bookish";
+            ViewBag.Data = data;
+            ViewBag.Type = new BookModel() { Id = 6, Author = "Author name" };
+
+            ViewData["Title"] = "WEB";
+            ViewData["Book"] = new BookModel() { Author = "hello", Id = 1 };
+
             return View();
         }
         public ViewResult Aboutus()
