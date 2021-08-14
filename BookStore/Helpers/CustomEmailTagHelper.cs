c0bf81d77a65d7bda619ae885cad3c5c1ff7cf78
psanthups <psanthups@gmail.com>
2021-08-14 17:16:21 +0530

    custom validation and custom taghelper

---
+++ BookStore/Helpers/CustomEmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Helpers
+{
+    public class CustomEmailTagHelper : TagHelper
+    {
+        public string MyEmail { get; set; }                                                        //here this property acts as a attribute
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";                                                                  //first we need to define the name of tag
+            output.Attributes.SetAttribute("href", $"mailto:{MyEmail}");                            //second we need to set the attribute (we can do this other way also that is in next line)
+            output.Attributes.Add("id", "my-email-id"); 
+            output.Content.SetContent("my-email");                                                 //third is how to set content
+        }
+    }
+}
